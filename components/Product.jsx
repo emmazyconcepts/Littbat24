c8e9a40d2c281007bcd4bf6a0f762ab34ec26e05
Your Name <emmazy100@gmail.com>
2023-01-28 16:33:32 +0100

    pages

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Link from 'next/link';
 
 import { urlFor } from '../lib/client';
 
 const Product = ({ product: { image, name, slug, price } }) => {
+
+
   return (
     <div>
-      <Link href={`/product/${slug}`}>
+      <Link href={`/product/${slug.current}`}>
         <div className="product-card">
           <img 
             src={urlFor(image && image[0])}
