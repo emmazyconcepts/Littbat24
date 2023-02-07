250442e3f039bde30ed3420db11b1ecb80265504
Your Name <emmazy100@gmail.com>
2023-02-07 14:45:40 +0100

    sanity

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import React from "react"
 
 
 export default function Home() {
   return (
    
-   <div>littbat274</div>
+   <>
+   Herobanner 
+
+
+
+
+   <div className='products-heading'>
+   <h2>Best Selling Batterys</h2>
+   <p>Batterys of many variations</p>
+   </div>
+
+   <div className='products-container'>
+   {["Product 1", "Product 2"].map (
+    (product) => product
+   )}
+   </div>
+  
+
+   </>
+   )
     
-  )
+  
 }
