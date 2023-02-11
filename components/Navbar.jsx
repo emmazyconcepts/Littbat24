1cfcb6cafac960f32f34eed7ea7ff849d0173d40
ekeemmanuel <emmazy100@gmail.com>
2023-02-11 12:12:45 +0100

    ‘add’

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,6 +4,14 @@ import { AiOutlineShopping } from "react-icons/ai";
 import { Cart } from "./";
 import { useStateContext } from "../context/StateContext";
 
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { Fragment } from "react";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
 const Navbar = () => {
   const { showCart, setShowCart, totalQuantities } = useStateContext();
 
@@ -15,7 +23,7 @@ const Navbar = () => {
 
       <div></div>
 
-      <select className="logo" name="" id="">
+      {/* <select className="logo" name="" id="">
         {" "}
         <option>Shop by department</option>
         <option>
@@ -28,11 +36,92 @@ const Navbar = () => {
         <option>380v batteries</option>
         <option>409v batteries</option>
         <option>6v batteries</option>
-      </select>
+      </select> */}
 
       <h1 className="logo">Todays deal</h1>
       <h1 className="logo">New Products</h1>
       <h1 className="logo">Offers</h1>
+
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button className=" inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+            Shop by department
+            <ChevronDownIcon
+              className="-mr-1 h-5 w-5 text-gray-400"
+              aria-hidden="true"
+            />
+          </Menu.Button>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="py-1">
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="/policy"
+                    className={classNames(
+                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                      "block px-4 py-2 text-sm"
+                    )}
+                  >
+                    24v batteries{" "}
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                      "block px-4 py-2 text-sm"
+                    )}
+                  >
+                    48v batteries{" "}
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                      "block px-4 py-2 text-sm"
+                    )}
+                  >
+                    512v batteries{" "}
+                  </a>
+                )}
+              </Menu.Item>
+              <form method="POST" action="#">
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      type="submit"
+                      className={classNames(
+                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                        "block w-full px-4 py-2 text-left text-sm"
+                      )}
+                    >
+                      380v batteries{" "}
+                    </button>
+                  )}
+                </Menu.Item>
+              </form>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
 
       <button
         type="button"
