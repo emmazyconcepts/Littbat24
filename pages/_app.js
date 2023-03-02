8136ca1d10958d8b57d1a510863f1322faffddee
ekeemmanuel <emmazy100@gmail.com>
2023-03-02 03:08:00 +0100

    cart

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 
-import { Layout } from '../components';
-import '../styles/globals.css';
+import { Layout } from "../components";
+import { Toaster } from "react-hot-toast";
+
+import "../styles/globals.css";
+import { StateContext } from "../context/StateContext";
 
 export default function App({ Component, pageProps }) {
-  return (  <Layout>
-  <Component {...pageProps} />
-</Layout>)
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
 }
