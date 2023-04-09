f99f6220fffa01983b2f6a2c12384be838d935e5
ekeemmanuel <emmazy100@gmail.com>
2023-04-09 18:52:09 +0100

    add

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,35 +1,38 @@
-import { urlFor } from '@/lib/client'
-import Link from 'next/link'
-import React, { useEffect } from 'react'
+import { urlFor } from "@/lib/client";
+import Link from "next/link";
+import React, { useEffect } from "react";
 
-const HeroBanner = ({HeroBanner}) => {
+import headimg from "../public/1678361828_products-background.jpeg";
+import Image from "next/image";
+
+const HeroBanner = ({ HeroBanner }) => {
   useEffect(() => {
-    console.log(HeroBanner.buttonText)
-  })
+    console.log(HeroBanner.buttonText);
+  });
   return (
-    <div className='hero-banner-container'>
-    
-    <div>
-    <p className='beats-solo'>{HeroBanner.smallText}</p>
-    <h3>{HeroBanner.midText}</h3>
-    <h1>{HeroBanner.largeText1}</h1>
-    <img src={urlFor(HeroBanner.image)} alt="battery" className="hero-banner-image" />
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">{HeroBanner.smallText}</p>
+        <h3>{HeroBanner.midText}</h3>
+        <h1>{HeroBanner.largeText1}</h1>
+        <Image
+          src={headimg}
+          alt="battery"
+          className="hero-banner-image w-full h-full"
+        />
 
-    <div>
-    <Link href={`/product/${HeroBanner.product}`}>
-    <button>
-   {HeroBanner.buttonText}
-    </button>
-    </Link>
-    <div className='desc'>
-    <h5>Description</h5>
-    <p>{HeroBanner.desc}</p>
-
+        <div>
+          <Link href={`/product/${HeroBanner.product}`}>
+            <button>{HeroBanner.buttonText}</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>{HeroBanner.desc}</p>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
+  );
+};
 
-    </div></div>
-  )
-}
-
-export default HeroBanner
+export default HeroBanner;
