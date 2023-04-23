c77fc78345e41b6ddee3c8e5a84161eeaf0cf84f
ekeemmanuel <emmazy100@gmail.com>
2023-04-23 15:33:50 +0100

    add

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -96,14 +96,14 @@ const Footer = () => {
         <div class="container mx-auto px-4">
           <div class="sm:flex sm:flex-wrap sm:-mx-4 md:py-4">
             <div class="px-4 sm:w-1/2 md:w-1/4 xl:w-1/6">
-              <h5 class="text-xl font-bold mb-6">Features</h5>
+              <h5 class="text-xl font-bold mb-6">Volts</h5>
               <ul class="list-none footer-links">
                 <li class="mb-2">
                   <a
                     href="#"
                     class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800"
                   >
-                    Cool stuff
+                    24v batteries{" "}
                   </a>
                 </li>
                 <li class="mb-2">
@@ -111,7 +111,7 @@ const Footer = () => {
                     href="#"
                     class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800"
                   >
-                    Random feature
+                    48v batteries{" "}
                   </a>
                 </li>
                 <li class="mb-2">
@@ -119,7 +119,7 @@ const Footer = () => {
                     href="#"
                     class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800"
                   >
-                    Team feature
+                    512v batteries{" "}
                   </a>
                 </li>
                 <li class="mb-2">
@@ -127,60 +127,36 @@ const Footer = () => {
                     href="#"
                     class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800"
                   >
-                    Stuff for developers
-                  </a>
-                </li>
-                <li class="mb-2">
-                  <a
-                    href="#"
-                    class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800"
-                  >
-                    Another one
-                  </a>
-                </li>
-                <li class="mb-2">
-                  <a
-                    href="#"
-                    class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800"
-                  >
-                    Last time
+                    380v batteries{" "}
                   </a>
                 </li>
               </ul>
             </div>
             <div class="px-4 sm:w-1/2 md:w-1/4 xl:w-1/6 mt-8 sm:mt-0">
-              <h5 class="text-xl font-bold mb-6">Resources</h5>
+              <h5 class="text-xl font-bold mb-6">Deals</h5>
               <ul class="list-none footer-links">
                 <li class="mb-2">
                   <a
-                    href="#"
+                    href={"/battery/todaydeal"}
                     class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800"
                   >
-                    Resource
+                    Todays deal
                   </a>
                 </li>
                 <li class="mb-2">
                   <a
-                    href="#"
+                    href={"/battery/newproduct"}
                     class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800"
                   >
-                    Resource name
+                    New Products
                   </a>
                 </li>
                 <li class="mb-2">
                   <a
-                    href="#"
+                    href={"/battery/offers"}
                     class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800"
                   >
-                    Another resource
-                  </a>
-                </li>
-                <li class="mb-2">
-                  <a
-                    href="#"
-                    class="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800"
-                  >
-                    Final resource
+                    Offers{" "}
                   </a>
                 </li>
               </ul>
@@ -280,7 +256,7 @@ const Footer = () => {
 
           <div class="sm:flex sm:flex-wrap sm:-mx-4 mt-6 pt-6 sm:mt-12 sm:pt-12 border-t">
             <div class="sm:w-full px-4 md:w-1/6">
-              <strong>FWR</strong>
+              <strong>Contact email</strong>
             </div>
             <div class="px-4 sm:w-1/2 md:w-1/4 mt-4 md:mt-0">
               <h6 class="font-bold mb-2">Address</h6>
@@ -291,15 +267,15 @@ const Footer = () => {
               </address>
             </div>
             <div class="px-4 sm:w-1/2 md:w-1/4 mt-4 md:mt-0">
-              <h6 class="font-bold mb-2">Free Resources</h6>
+              <h6 class="font-bold mb-2">About Us </h6>
               <p class="mb-4 text-sm">
-                Use our HTML blocks for <strong>FREE</strong>.<br />
-                <em>All are MIT License</em>
+                LitBatt24 prides itself for offering a comprehensive selection
+                of quality LiFePO4 batteries to our customers.
               </p>
             </div>
             <div class="px-4 md:w-1/4 md:ml-auto mt-6 sm:mt-4 md:mt-0">
               <button class="px-4 py-2 bg-purple-800 hover:bg-purple-900 rounded text-white">
-                Get Started
+                Shop Now
               </button>
             </div>
           </div>
