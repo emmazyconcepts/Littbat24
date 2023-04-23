05ce6434bfc33c22af046acf04f2aaff77bde811
ekeemmanuel <emmazy100@gmail.com>
2023-04-23 15:52:02 +0100

    add

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -14,11 +14,7 @@ const Footer = () => {
           <div class="w-full md:w-1/2 lg:w-1/4 pl-5 pr-5 mb-5 lg:pl-2 lg:pr-2">
             <div class="bg-white rounded-lg m-h-64 p-2 transform hover:translate-y-2 hover:shadow-xl transition duration-300">
               <figure class="mb-2">
-                <img
-                  src="https://srv-cdn.onedio.com/store/bf2cbc886120f284ef46fd92a48f5fb58c62e6a50fbdf8fa796d057dd0ddc242.png"
-                  alt=""
-                  class="h-64 ml-auto mr-auto"
-                />
+                <img src="/bike.png" alt="" class="h-64 ml-auto mr-auto" />
               </figure>
               <div class="rounded-lg p-4 bg-purple-700 flex flex-col">
                 <div>
@@ -30,28 +26,23 @@ const Footer = () => {
                     product faster, you can opt for next day delivery..
                   </span>
                 </div>
-                <div class="flex items-center">
-                  <div class="text-lg text-white font-light">$1099,00</div>
-                  <button
-                    href="javascript:;"
-                    class="rounded-full bg-purple-900 text-white hover:bg-white hover:text-purple-900 hover:shadow-xl focus:outline-none w-10 h-10 flex ml-auto transition duration-300"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="stroke-current m-auto"
-                    >
-                      <line x1="12" y1="5" x2="12" y2="19"></line>
-                      <line x1="5" y1="12" x2="19" y2="12"></line>
-                    </svg>
-                  </button>
+              </div>
+            </div>
+          </div>
+          <div class="w-full md:w-1/2 lg:w-1/4 pl-5 pr-5 mb-5 lg:pl-2 lg:pr-2">
+            <div class="bg-white rounded-lg m-h-64 p-2 transform hover:translate-y-2 hover:shadow-xl transition duration-300">
+              <figure class="mb-2">
+                <img src="/cash.png" alt="" class="h-64 ml-auto mr-auto" />
+              </figure>
+              <div class="rounded-lg p-4 bg-purple-600 flex flex-col">
+                <div>
+                  <h5 class="text-white text-2xl font-bold leading-none">
+                    Cash On Delivery
+                  </h5>
+                  <span class="text-xs text-gray-400 leading-none">
+                    Cash on Delivery is available as a payment method for all
+                    items that are Fulfilled by LitBatt24{" "}
+                  </span>
                 </div>
               </div>
             </div>
@@ -60,25 +51,25 @@ const Footer = () => {
             <div class="bg-white rounded-lg m-h-64 p-2 transform hover:translate-y-2 hover:shadow-xl transition duration-300">
               <figure class="mb-2">
                 <img
-                  src="https://srv-cdn.onedio.com/store/7fd6410ffac110960cb4a60b09878db19ebbdf3c8b6e842918d16f3c61784763.png"
+                  src="https://srv-cdn.onedio.com/store/988bccbdb9ca395f581f98faa9ce3a55123f12bfcef608c838532b813646e557.png"
                   alt=""
                   class="h-64 ml-auto mr-auto"
                 />
               </figure>
-              <div class="rounded-lg p-4 bg-purple-600 flex flex-col">
+              <div class="rounded-lg p-4 bg-gray-700 flex flex-col">
                 <div>
                   <h5 class="text-white text-2xl font-bold leading-none">
-                    iPhone 11
+                    iPhone XR
                   </h5>
                   <span class="text-xs text-gray-400 leading-none">
-                    Just the right amount of everything.
+                    Brilliant. In every way.
                   </span>
                 </div>
                 <div class="flex items-center">
-                  <div class="text-lg text-white font-light">$699,00</div>
+                  <div class="text-lg text-white font-light">$599,00</div>
                   <button
                     href="javascript:;"
-                    class="rounded-full bg-purple-800 text-white hover:bg-white hover:text-purple-900 hover:shadow-xl focus:outline-none w-10 h-10 flex ml-auto transition duration-300"
+                    class="rounded-full bg-gray-900 text-white hover:bg-white hover:text-purple-900 hover:shadow-xl focus:outline-none w-10 h-10 flex ml-auto transition duration-300"
                   >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
