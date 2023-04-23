e1ab1f502e8591e9a66ea0ac9288e32ddff3f663
ekeemmanuel <emmazy100@gmail.com>
2023-04-23 15:10:35 +0100

    ffavi

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 
 import { Layout } from "../components";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 import "../styles/globals.css";
 import { StateContext } from "../context/StateContext";
@@ -9,6 +10,9 @@ import { StateContext } from "../context/StateContext";
 export default function App({ Component, pageProps }) {
   return (
     <StateContext>
+      <Head>
+        <link rel="shortcut icon" href="/1678107511_litbatt24_logo_8.jpeg" />
+      </Head>
       <Layout>
         <Toaster />
         <Component {...pageProps} />
